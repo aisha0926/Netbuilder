d399c1ee66a352ed2fb3c67b09c97ff34c285840
aisha0926 <aisha.0926@hotmail.com>
2018-09-03 22:32:00 +0100

    Fixing re-work

--- src/main/java/FamilyTask/Family.java
+++ src/main/java/FamilyTask/Family.java
@@ -4,39 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Family {
-	Map<String, FamilyModel> family;
-	
-	public Family() {
-		family = new HashMap<String, FamilyModel>();
-	}
-
-	public void creatingFamily(){
-		FamilyModel newFamily = new FamilyModel("");
-	}
-	
-	public boolean male(String name) {
-		
-		return false;
-	}
-	
-	public boolean female(String name) {
-		return false;
-	}
-	
-	public boolean isMale(String name) {
-		return false;
-	}
-	
-	public boolean isFemale(String name) {
-		return false;
-	}
-	
-	public void setParentOf(String childName, String parentName) {
-		
-	}
-	
-	public String getParentOf(String parentName) {
-		return parentName;
+	public static void main(String[] args) {
+		FamilyModel a = new FamilyModel();
+		a.setName("George");
+		a.setGender(Gender.MALE);
 	}
 	
 
