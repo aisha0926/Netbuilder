3114a32094a228e2c34f1d749e91e5c72ae878a6
aisha0926 <aisha.0926@hotmail.com>
2018-09-03 17:01:30 +0100

    Creating string manipulation

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String stringValue = "This NETBUILDER assessment is way too easy.12312";
+        char[] convertedCharacters = stringValue.toCharArray();
+        for (char loopingThroughTheArray : convertedCharacters) {
+            int individualStringValues = (int) loopingThroughTheArray;
+            int asciiLowerCaseStartingPoint = 96;
+            int asciiUpperCaseStartingPoint = 64;
+            if (individualStringValues <= 122 & individualStringValues >= 97){
+                System.out.print(" ");
+                System.out.print(individualStringValues - asciiLowerCaseStartingPoint);
+            } else if(individualStringValues <= 90 & individualStringValues >= 65) {
+                System.out.print(" ");
+                System.out.print(individualStringValues - asciiUpperCaseStartingPoint);
+            }
+        }
+
+    }
+
+}
