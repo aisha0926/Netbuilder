b999851c575eb1a5e797c7bfa56cda0ef390b06d
aisha0926 <aisha.0926@hotmail.com>
2018-09-05 08:38:06 +0100

    Test

--- src/main/java/StringManipulation/StringManipulationClass.java
+++ src/main/java/StringManipulation/StringManipulationClass.java
@@ -1,25 +1,55 @@
 package StringManipulation;
 
+import java.util.ArrayList;
+
 public class StringManipulationClass {
 
-	public static void main  (String[] args) {
-		 String stringValue = "This NETBUILDER assessment is way too easy.12312";
-	        char[] convertedCharacters = stringValue.toCharArray();
-	        
-	        for (char loopingThroughTheArray : convertedCharacters) {
-	            int individualStringValues = (int) loopingThroughTheArray;
-	            int asciiLowerCaseStartingPoint = 96;
-	            int asciiUpperCaseStartingPoint = 64;
-	            
-	            
-	            if (individualStringValues <= 122 & individualStringValues >= 97){
-	                System.out.print(" ");
-	                System.out.print(individualStringValues - asciiLowerCaseStartingPoint);
-	            } 
-	            else if(individualStringValues <= 90 & individualStringValues >= 65) {
-	                System.out.print(" ");
-	                System.out.print(individualStringValues - asciiUpperCaseStartingPoint);
-	            }
-	        }
+//	public static void main  (String[] args) {
+//		 String stringValue = "This NETBUILDER assessment is way too easy.12312";
+//	        char[] convertedCharacters = stringValue.toCharArray();
+//	        
+//	        for (char loopingThroughTheArray : convertedCharacters) {
+//	            int individualStringValues = (int) loopingThroughTheArray;
+//	            int asciiLowerCaseStartingPoint = 96;
+//	            int asciiUpperCaseStartingPoint = 64;
+//	            
+//	            
+//	            if (individualStringValues <= 122 & individualStringValues >= 97){
+//	                System.out.print(" ");
+//	                System.out.print(individualStringValues - asciiLowerCaseStartingPoint);
+//	            } 
+//	            else if(individualStringValues <= 90 & individualStringValues >= 65) {
+//	                System.out.print(" ");
+//	                System.out.print(individualStringValues - asciiUpperCaseStartingPoint);
+//	            }
+//	        }
+//		}
+	
+	public ArrayList<String> stringManipulation(String stringValue) {
+		char[] convertedCharacters = stringValue.toCharArray();
+		ArrayList<Integer> characterList = new ArrayList<Integer>();
+		ArrayList<String> array = new ArrayList<String>();
+		
+		for (char loopingThroughTheArray : convertedCharacters) {
+            int individualStringValues = (int) loopingThroughTheArray;
+            int asciiLowerCaseStartingPoint = 96;
+            int asciiUpperCaseStartingPoint = 64;
+            int test=0;
+            
+            if (individualStringValues <= 122 & individualStringValues >= 97){
+            	test = individualStringValues - asciiLowerCaseStartingPoint;
+            	characterList.add(0, test);
+            	array.add(String.valueOf(test));
+                
+            } 
+            else if(individualStringValues <= 90 & individualStringValues >= 65) {
+            	test = individualStringValues - asciiUpperCaseStartingPoint;
+            	characterList.add(0, test);
+            	array.add(String.valueOf(test));
+               
+            }
+        }
+		
+		return array;
 	}
 }
--- src/test/java/StringManipulationTest.java
+++ src/test/java/StringManipulationTest.java
@@ -8,11 +8,11 @@ public class StringManipulationTest {
 	
 	@Test
 	public void test() {
-		StringManipulationClass str = new StringManipulationClass("This NETbuilder assessment is way too easy.");
-	       str.manipulation();
-	       System.out.print(str);
+		StringManipulationClass str = new StringManipulationClass();
+			//str.stringManipulation("This NETbuilder assessment is way too easy.");
+	       System.out.print(str.stringManipulation("This NETbuilder assessment is way too easy."));
 	       
-	       //assertEquals("2089191452022191245181191951919135142091923125201515511925", str);
+	       assertEquals("20 8 9 19 14 5 20 2 21 9 12 4 5 18 1 19 19 5 19 19 13 5 14 20 9 19 23 1 25 20 15 15 5 1 19 25 ", str.toString());
 	}
 
 }
